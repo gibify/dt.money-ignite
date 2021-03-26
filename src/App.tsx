f9544f17c46b272730f0d0df2f2ffd4cae6650a3
gibifyofficial <evandrogibicoski@outlook.com>
2021-03-26 10:02:27 -0300

    add provider and db config

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Header } from './components/Header';
 import { NewTransactionModal } from './components/NewTransactionModal';
 
 import { GlobalStyle } from './styles/global';
+import { TransactionProvider } from './TransactionContext';
 
 Modal.setAppElement('#root');
 
@@ -21,7 +22,7 @@ export function App() {
   }
 
   return (
-    <> 
+    <TransactionProvider > 
       <Header 
       onOpenNewTransactionModal={handleOpenNewTransactionModal}
       />
@@ -34,7 +35,7 @@ export function App() {
       />
       
       <GlobalStyle />
-   </>
+   </TransactionProvider>
   );
 }
 
